89265a74075a76ba39d328a3102e362c0efcbd8f
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-06-05 19:11:40 +0300

    Create some components and include header component to root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
+import { ShoppingListEditComponent } from './components/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
