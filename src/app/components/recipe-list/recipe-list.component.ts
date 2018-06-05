ac3bb50d47e282a9b9f59fae3c8a55096377e3a9
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-06-05 20:32:27 +0300

    Create recipe model and fill recipe list

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../../models/Recipe';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test', 'This is a test recipe', 'https://images.pexels.com/photos/23086/food-restaurant-kitchen-meat-23086.jpg'),
+    new Recipe('A test', 'This is a second test recipe', 'https://images.pexels.com/photos/76093/pexels-photo-76093.jpeg'),
+    new Recipe('A test', 'This is a third test recipe', 'https://images.pexels.com/photos/156114/pexels-photo-156114.jpeg')
 
+  ];
   constructor() { }
 
   ngOnInit() {
